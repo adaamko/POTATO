2cb6d295e40c1a397fb75394326a0a864a13da11
Adam Kovacs <adaam.ko@gmail.com>
2022-03-08 14:33:17 +0100

    POTATO release v0.1.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="xpotato",
-    version="0.0.9",
+    version="0.1.0",
     description="XAI human-in-the-loop information extraction framework",
     long_description=open("README.md", "r", encoding="utf-8").read(),
     long_description_content_type="text/markdown",
