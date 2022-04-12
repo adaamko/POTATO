475081b46fca8518e502ea7c3f00955ea4ca0619
Adam Kovacs <adaam.ko@gmail.com>
2022-04-12 10:00:48 +0200

    Extractor return subgraph

--- xpotato/graph_extractor/extract.py
+++ xpotato/graph_extractor/extract.py
@@ -154,7 +154,7 @@ class FeatureEvaluator:
         matcher = GraphFormulaMatcher(features, converter=default_pn_to_graph)
 
         for i, g in tqdm(enumerate(graphs)):
-            feats = matcher.match(g, return_subgraphs=return_subgraphs)
+            feats = matcher.match(g, return_subgraphs=True)
             if multi:
                 self.match_multi(feats, features, matches, predicted, matched_graphs)
             else:
