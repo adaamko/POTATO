14c31bd649c67a5ec3fc4aacc8ce405f17839c67
GKingA <kinga.andrea.gemes@gmail.com>
2022-04-19 16:43:07 +0200

    Explainable dataset update

--- xpotato/dataset/explainable_dataset.py
+++ xpotato/dataset/explainable_dataset.py
@@ -1,18 +1,54 @@
 from typing import Dict, List, Tuple
 
 import pandas as pd
+from tqdm import tqdm
+
 from xpotato.dataset.dataset import Dataset
 from xpotato.dataset.explainable_sample import ExplainableSample
+from xpotato.graph_extractor.graph import PotatoGraph
 
 
 class ExplainableDataset(Dataset):
     def __init__(
-        self, examples: List[Tuple[str, str]], label_vocab: Dict[str, int], lang="en"
+        self,
+            examples: List[Tuple[str, str]] = None,
+            label_vocab: Dict[str, int] = {}, lang="en",
+            path=None,
+            binary=False,
+            cache_dir=None,
+            cache_fn=None,
     ) -> None:
-        super().__init__(examples=examples, label_vocab=label_vocab, lang=lang)
+        super().__init__(examples=examples, label_vocab=label_vocab, lang=lang, path=path, binary=binary,
+                         cache_dir=cache_dir, cache_fn=cache_fn)
 
-    def read_dataset(self, examples: List[Tuple[str, str]]) -> List[ExplainableSample]:
-        return [ExplainableSample(example) for example in examples]
+    def read_dataset(
+        self,
+        examples: List[Tuple[str, str]] = None,
+        path: str = None,
+        binary: bool = False,
+    ) -> List[ExplainableSample]:
+        if examples:
+            return [ExplainableSample(example) for example in examples]
+        elif path:
+            if binary:
+                df = pd.read_pickle(path)
+                graphs_str = self.prune_graphs(df.graph.tolist())
+                df.drop(columns=["graph"], inplace=True)
+                df["graph"] = graphs_str
+            else:
+                df = pd.read_csv(path, sep="\t")
+            samples = [
+                ExplainableSample(
+                    (example["text"], example["label"], example["rationale"]),
+                    potato_graph=PotatoGraph(graph_str=example["graph"]),
+                    label_id=example["label_id"],
+                )
+                for _, example in tqdm(df.iterrows())
+            ]
+            self.graphs = [sample.potato_graph.graph for sample in samples]
+            return samples
+        else:
+            raise ValueError("No examples or path provided")
 
     def to_dataframe(self, as_penman: bool = False) -> pd.DataFrame:
         df = pd.DataFrame(
